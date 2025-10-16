f516433b6bf35d1dbd9f6378432b19c4247c30c2
Mayank Gupta <mayankgupta270606@gmail.com>
2025-10-16 18:31:03 +0530

    INHERITENCE IMPLIED

--- main.py
+++ main.py
@@ -26,9 +26,12 @@
 #     @classmethod# class method can only access class variable
 #     def get_wheels_needed(cls):#cls is a convention for class method cls refers to class as self refers to object
 #         return cls.total_cars * 4
-#     @staticmethod
-#     def total_cars_count():# no need to pass self or cls
-#         return Car.total_cars
+    # @staticmethod
+    # def total_cars_count():# no need to pass self or cls
+    #     return Car.total_cars
+    # @staticmethod
+    # def printmessage():
+    #     print("Cars are cool!")  
 
     
 
@@ -52,3 +55,23 @@
 
 # print(Car.total_cars)#accsessing class variable
 
+#  INHERITENCE
+
+# class Car:#class
+#     def __init__(self, make, model, year):#constructor
+#         self.make = make
+#         self.model = model
+#         self.year = year
+
+#     def display_info(self):
+#         return f"{self.year} {self.make} {self.model}"
+    
+# class ElectricCar(Car):#child class
+#     def __init__(self, make, model, year, battery_size):#constructor
+#         super().__init__(make, model, year)#calling parent class constructor
+#         self.battery_size = battery_size
+#     def display_battery_info(self):# acess parent class method
+#         return f"{self.display_info()} with a {self.battery_size}-kWh battery"
+
+# obj1 = ElectricCar("Tesla", "Model S", 2020, 100)#object    
+# print(obj1.display_battery_info())
